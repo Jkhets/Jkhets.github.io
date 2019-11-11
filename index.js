b9f7027aa94af999e72fc6aeb8692e42537b9193
Jay Khetia <Jkhets@gmail.com>
2019-11-11 11:42:30 +0000

    more js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-elem = document.createElement("div");
-elem.id = "myID";
-elem.innerHTML = "This div was made with JavaScript";
-document.body.insertBefore(elem, document.body.childNodes[0]);
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    elem = document.createElement("div");
+    elem.id = "myID";
+    elem.innerHTML = "This div was made with JavaScript";
+    document.body.insertBefore(elem, document.body.childNodes[0]);
+  },
+  false
+);
